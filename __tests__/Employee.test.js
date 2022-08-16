aee3a679238dfa3e6a9357303e1beafe0845fc72
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-08-16 10:50:45 -0400

    employee ID test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,6 +8,6 @@ test('Set name of employee', () => {
 
 test('Set employee Id', () => {
     const id = "1";
-    const employee = new Employee(name);
+    const employee = new Employee("Bill Lumbergh", id, "bill.lumbergh@initech.com");
     expect(employee.id).toBe(id);
 });
